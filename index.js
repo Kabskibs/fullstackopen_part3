c687743238b1230c165739d77d572c1ec76afcd5
Kabskibs <kabs_dev@proton.me>
2024-06-22 05:04:56 +0300

    commit 3.12 - 3.18

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 var morgan = require('morgan')
 const cors = require('cors')
+const Phonebook = require('./models/mongo_phonebook')
 const app = express()
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
 
 morgan.token('req-body', request => {
     return JSON.stringify(request.body)
@@ -15,7 +17,7 @@ app.use(morgan(
     ':method :url :status :res[content-length] - :response-time ms :req-body'
 ))
 
-let persons = [
+/* let persons = [
     {
         "id": 1,
         "name": "Arto Hellas",
@@ -36,72 +38,110 @@ let persons = [
         "name": "Mary Poppendick",
         "number": "39-23-6423122"
     }
-]
-
-const generateId = () => {
-    return Math.floor(Math.random() * 999999999)
-}
+] */
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
-
-app.get('/info', (request, response) => {
+app.get('/info', async (request, response) => {
+    const totalCount = await Phonebook.countDocuments({})
     const currentDate = new Date()
     response.send(
-        `<p>Phonebook has info for ${persons.length} people</p>
+        `<p>Phonebook has info for ${totalCount} people</p>
         <p>${currentDate}</p>`
     )
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons', (request, response) => {
+    Phonebook.find({})
+        .then(result => {
+            if (result) {
+                response.json(result)
+            } else {
+                response.status(404).end()
+            }
+        })
+})
+
+
+app.get('/api/persons/:id', (request, response, next) => {
+    Phonebook.findById(request.params.id)
+        .then(result => {
+            if (result) {
+                response.json(result)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    if (!body.name || !body.number) {
+    if (body.name === undefined) {
         return response.status(400).json({
-            error: 'content missing'
+            error: 'name missing'
         })
     }
-    if (persons.some(p => p.name === body.name)) {
+    /* if (persons.some(p => p.name === body.name)) {
         return response.status(409).json({
             error: 'name must be unique'
         })
-    }
-    const person = {
+    } */
+    const person = new Phonebook({
         name: body.name,
         number: body.number,
-        id: generateId()
-    }
-    persons = persons.concat(person)
-    response.json(person)
-})
-
-
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
-    persons = persons.filter(p => p.id !== id)
-    response.status(200).json({
-        name: person.name,
-        id: person.id
+    })
+    person.save()
+        .then(result => {
+            response.json(result)
     })
 })
 
-const PORT = process.env.PORT || 3001
+// Doesn't work, find out why
+app.put('/api/persons/:id', (request, response, next) => {
+    const reqBody = request.body
+    const updatedBody = {
+        name: reqBody.name,
+        number: reqBody.number
+    }
+    Phonebook.findByIdAndUpdate(request.params.id, updatedBody, { new: true })
+        .then(result => {
+            if (result) {
+                response.status(200).json(result)
+            }
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Phonebook.findByIdAndDelete(request.params.id)
+        .then(result => {
+            if (result) {
+                response.status(200).json(result)
+            } else {
+                response.status(204).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
